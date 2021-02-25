77b87c1bb8ef10f031345e4bbe2862edd7151e5f
Gourab karmakar <gourabkarmakar297@gmail.com>
2021-02-25 23:01:11 +0530

    Add command

--- main.py
+++ main.py
@@ -32,26 +32,34 @@ class Qrgernerator:
         lbl_emp_ano=Label(emp_frame,text="Aadhar No",font=("times new roman",15,'bold'),bg='white')
         lbl_emp_ano.place(x=25,y=220)
 
-        # Entry
+        # =================== Entry ==========================
 
-        ent_emp_code=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow')
+
+        self.var_emp_code=StringVar()
+        self.var_emp_fname=StringVar()
+        self.var_emp_lname=StringVar()
+        self.var_emp_mno=StringVar()
+        self.var_emp_ano=StringVar()
+    
+
+        ent_emp_code=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow',textvar=self.var_emp_code)
         ent_emp_code.place(x=185,y=60)
 
-        ent_emp_fname=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow')
+        ent_emp_fname=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow',textvar=self.var_emp_fname)
         ent_emp_fname.place(x=185,y=100)
 
-        ent_emp_lname=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow')
+        ent_emp_lname=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow',textvar=self.var_emp_lname)
         ent_emp_lname.place(x=185,y=140)
 
-        ent_emp_mno=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow')
+        ent_emp_mno=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow',textvar=self.var_emp_mno)
         ent_emp_mno.place(x=185,y=180)
 
-        ent_emp_ano=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow')
+        ent_emp_ano=Entry(emp_frame,font=("times new roman",15,'bold'),bg='lightyellow',textvar=self.var_emp_ano)
         ent_emp_ano.place(x=185,y=220)
 
         # ========= Button ===========
 
-        btn_save=Button(emp_frame,text="Save",bg="#497e1e",fg="white",font=("times new roman",15,'bold'))
+        btn_save=Button(emp_frame,text="Save",bg="#497e1e",fg="white",font=("times new roman",15,'bold'),command=self.generate)
         btn_save.place(x=25,y=270,width=90,height=40)
 
         btn_clear=Button(emp_frame,text="Clear",bg="#497e1e",fg="white",font=("times new roman",15,'bold'))
@@ -65,11 +73,15 @@ class Qrgernerator:
         qr_frame=Frame(self.root,bd=2,relief=RIDGE,bg='white')
         qr_frame.place(x=560,y=90,width=300,height=390)
 
-        emp_title=Label(qr_frame,text="Qr Show",font=("goudy old style",20),bg=primary_color,fg="white")
-        emp_title.place(x=0,y=0,relwidth=1)
-        
+        qr_title=Label(qr_frame,text="Qr Show",font=("goudy old style",20),bg=primary_color,fg="white")
+        qr_title.place(x=0,y=0,relwidth=1)       
 
-        # self.qr_frame=Label()
+        self.qr_frame=Label(qr_frame,text="No qr Available", font=('times new roman',15),bg='#bd3333',fg="white")
+        self.qr_frame.place(x=60,y=100,width=180,height=180)
+
+    def generate(self):
+        print(self.var_emp_code.get())
+            
 
 
 root=Tk()
